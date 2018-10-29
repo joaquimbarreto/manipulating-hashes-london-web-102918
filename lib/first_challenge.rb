fa5add5a10bb4baf6a18b0c945e8624480d84db1
Joaquim Barreto <joaquim@barreto.co>
2018-10-29 23:17:42 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   #your code here
 
-contacts["Jon Snow"][:favorite_icecream_flavors].delete_if
+contacts["Jon Snow"][:favorite_icecream_flavors].delete_if {|
 
   #remember to return your newly altered contacts hash!
   contacts
